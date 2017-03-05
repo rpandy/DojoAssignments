e7c389cadaf2fd46c0e67cde6008ade13a1509ca
Robert Pandy <rspandy@gmail.com>
2017-03-05 11:45:16 -0600

    API and AJAX updates

--- WebFundamentals/APIsAndAjax/GoT/GoT.js
+++ WebFundamentals/APIsAndAjax/GoT/GoT.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
-    $('form').submit(function() {
-        // your code here (build up your url)
-        $.get(url, function(res) {
-            // your code here
+    $('#houses').on('click','img',function() {
+        var houseID = $(this).attr('id')
+        console.log(houseID)
+        $.get("http://anapioficeandfire.com/api/houses/" + houseID, function(res) {
+            var houseTitles = []
+            for(var i = 1; i < res.titles.length; i++){
+                console.log(res.titles[i])
+                houseTitles += res.titles[i]+","
+            }
+            var houseName = res.name
+            var houseWords = res.words
+            var string = '<h2>House Details</h2>'
+            string += '<p>Name:</p>'+ houseName
+            string += '<p>Words:</p>' + houseWords
+            string += '<p>Titles:' + houseTitles +'</p>'
+
+            console.log(string)
+            console.log(res)
+            console.log(res.name)
+            console.log(res.words)
+
+            $('#house_details').html(string)
         }, 'json');
-        // don't forget to return false so the page doesn't refresh
-        return false;
     });
 });
