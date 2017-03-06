c468412726c2c2c460509155b9b9be7414d4c3ed
Robert Pandy <rspandy@gmail.com>
2017-03-06 15:26:07 -0600

    python work

---
+++ Python/tryPython.py
@@ -0,0 +1,26 @@
+#create a variable and print it
+test = "you need to get more creative with your variable names"
+print test
+test = "anything other than 'test' would suffice"
+print test
+
+#print a mathematical operation
+
+beans = 2*5
+print beans
+
+
+#add 2 strings togehter
+x = "2 legit"
+y = " 2 legit 2 quit (HAY HAY!!)"
+
+z = x+y
+print z
+
+num = 2
+
+#combined a string and number 2 different ways. w/ concatenation and then with comma
+concat = str(num) + x
+concat = str(num),x
+
+print concat
