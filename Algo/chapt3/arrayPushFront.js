757dadf5785301f527325252682a0afa08d1ea68
Robert Pandy <rspandy@gmail.com>
2017-03-07 10:19:34 -0600

    Algo practice

--- Algo/chapt3/arrayPushFront.js
+++ Algo/chapt3/arrayPushFront.js
@@ -1,3 +1,10 @@
-function functionName() {
+// Array: Push Front
+// Given an array and an additional value, insert this value at the beginning of the array.
+// Do this without using any built-in array methods.
 
+function arrayPushFront(arr, num) {
+    console.log( arr + " before we add " + num + " to index 0")
+    arr[0] = (num)
+    console.log(arr)
 }
+arrayPushFront([1,2,3,4,5,6], 7)
