aa81b93153d76f834f1296dad34a6ab9a0a56ffa
Robert Pandy <rspandy@gmail.com>
2017-03-08 08:34:05 -0600

    python assignments

--- Python/coinTosses.py
+++ Python/coinTosses.py
@@ -1,3 +1,22 @@
-import random
-random_num = random.random()
-print random_num
+
+
+def coinTosses():
+    headCount = 0
+    tailCount = 0
+    print "Starting program"
+    for i in range(1,5000):
+        import random
+        randNum = random.random()
+        randNum = round(randNum)
+        #print randNum
+        if (randNum == 1):
+            randNum = "heads"
+            headCount = headCount + 1
+            print "Attempt #",i,"Throwing a coin... It's a head! ... Got",headCount,"head(s) so far and",tailCount,"tail(s) so far"
+        else:
+            randNum = "tails"
+            tailCount = tailCount + 1
+            print "Attempt #",i,"Throwing a coin... It's a tail! ... Got",headCount,"head(s) so far and",tailCount,"tail(s) so far"
+    print "End of program, thank you!"
+
+coinTosses()
