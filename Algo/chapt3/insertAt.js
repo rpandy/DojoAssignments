0d155ca8a19c1b7971568928a28ec0858b9599e2
Robert Pandy <rspandy@gmail.com>
2017-03-08 07:55:23 -0600

    Algo practice

--- Algo/chapt3/insertAt.js
+++ Algo/chapt3/insertAt.js
@@ -7,8 +7,10 @@ function insertAt(arr,idx,num) {
 var newArr = []
     for(var i = 0; i < arr.length; i++){
         if(idx === i){
+            //we use the index and the iteration to determine whether or not to push the new num (999)
             newArr.push(num)
         }
+        //if the idx and i != then we can push the old value into newArr.
         else newArr.push(arr[i])
     }
     console.log(newArr)
