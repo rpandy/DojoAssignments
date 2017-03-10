e141c298a56e5805bb51f2dcd7944ca7e1b3b8c4
Robert Pandy <rspandy@gmail.com>
2017-03-09 21:39:33 -0600

    Python assignments

--- Python/basicRegex.py
+++ Python/basicRegex.py
@@ -1,23 +1,37 @@
 
 pattern1 = r"v"
+
 #the + looks for instances where the linked characters are found together
 pattern2 = r"s+s"
+
 #\w* allows word characters in front of e
 #\b ensures that it is the last letter in the word. \b is a boundary assertion
 pattern3 = r"\w*e\b"
 
-pattern4 = r"b[^b]"
+# . allows us to select a pattern with a b (random character in the middle) b
+pattern4 = r"b.b"
+# .+ allows us to select a pattern with a b (one or more random characters in the middle) b
+
+pattern5 = r"b.+b"
+
+pattern6 = r"b.?b"
+
+pattern7 = r"(aeiou)"
+
+pattern8 = r"b.b"
+
+pattern9 = r"[a-z]*([a-z])\1([a-z])\2[a-z]*"
 
 import re
 def getMatchingWords(regex):
     words = ["aimlessness", "assassin", "baby", "beekeeper", "belladonna", "cannonball", "crybaby", "denver", "embraceable",
     "facetious", "flashbulb", "gaslight", "hobgoblin", "iconoclast", "issue", "kebab", "kilo", "laundered", "mattress",
      "natural", "obsessive", "paranoia", "queen", "rabble", "reabsorb", "sacrilegious", "schoolroom", "tabby", "tabloid",
-     "unbearable", "union", "videotape"]
+     "unbearable", "union", "videotape", "aeiou",]
     matches = []
 
     for word in words:
         if re.search(regex, word):
             matches.append(word)
     return matches
-print getMatchingWords(pattern3)
+print getMatchingWords(pattern9)
--- Python/pythonPractice.py
+++ Python/pythonPractice.py
@@ -1,54 +1,114 @@
-#display dictionaries on different lines to differentiate the key:value pairs
+# #display dictionaries on different lines to differentiate the key:value pairs
+#
+# name_dictionary = {
+#      "sw":"Sara Wong",
+#      "mp":"Martin Puryear"
+#      }
+# for key in name_dictionary:
+#     print name_dictionary[key]
+#
+# name_dictionary = {
+#      "sw":"Sara Wong",
+#      "mp":"Martin Puryear"
+#      }
+# for key, val in name_dictionary.items():
+#     print key, val
+#
+# name_dictionary = {
+#      "sw":"Sara Wong",
+#      "mp":"Martin Puryear"
+#      }
+# for key, val in name_dictionary.items():
+#     print name_dictionary["sw"]
+# #same as using name_dictionary.item()
+# name_dictionary = {
+#      "sw":"Sara Wong",
+#      "mp":"Martin Puryear"
+#      }
+# for key in name_dictionary:
+#     print key, name_dictionary[key]  #printing key of sw or mp and the name_dictionary of sw and mp as Sara Wong and Martin Puryear respectively
+#
+#
+#
+#
+# context = {
+#     'questions': [
+#     {'id':1, 'content': "Why is there a light in the fridge and not in the freezer?"},
+#     {'id':2, 'content': "Why don't sheep shrink when it rains?"},
+#     {'id':3, 'content': "Why are they called apartments when they are all stuck together?"},
+#     {'id':4, 'content': "Why do cars drive on the parkway and park on the driveway?"}
+#     ]
+# }
+#
+# for key, data in context.items():
+#     for value in data:
+#         print "Question #",value["id"],":",value["content"]
+#         print "-------"
+#
+#
+# name_dictionary = {
+#     "sw":"Sara Wong",
+#     "mp":"Martin Puryear"
+#     }
+# for key in name_dictionary:
+#     print name_dictionary[key]
+print "**------------------**"
 
-name_dictionary = {
-     "sw":"Sara Wong",
-     "mp":"Martin Puryear"
-     }
-for key in name_dictionary:
-    print name_dictionary[key]
+#iteration practice
+superheros = {
+    "quickster": "The Flash",
+    "buster": "The Hulk",
+    "flyer": "Silver Surfer",
+    "detective": "Bat Man",
+}
+print "**------------------**"
 
-name_dictionary = {
-     "sw":"Sara Wong",
-     "mp":"Martin Puryear"
-     }
-for key, val in name_dictionary.items():
-    print key, val
+#practice from lecture three session 1
 
-name_dictionary = {
-     "sw":"Sara Wong",
-     "mp":"Martin Puryear"
-     }
-for key, val in name_dictionary.items():
-    print name_dictionary["sw"]
-#same as using name_dictionary.item()
-name_dictionary = {
-     "sw":"Sara Wong",
-     "mp":"Martin Puryear"
-     }
-for key in name_dictionary:
-    print key, name_dictionary[key]  #printing key of sw or mp and the name_dictionary of sw and mp as Sara Wong and Martin Puryear respectively
+print superheros
+#we want the flash. still print superheros because we're looking at that dictionary
+#the brackets [] with the key in them get us the value we want
+print superheros["quickster"]
+#when dealing with superheros and for loops. hero is the key of the dictionary.
+for hero in superheros:
+    print "the key is ", hero
+    print "the value for", hero, "is", superheros[hero]
 
 
+print "**------------------**"
+#items
+for hero in superheros.items():
+    print hero
+#unpacked tuple into two variables.
+for key, value in superheros.items():
+    print key,value
+for key, value in superheros.items():
+        print key * 3,value
+print "**------------------**"
+#iteritems
+#only difference is on the backend and how the memory is cached
+for key, value in superheros.iteritems():
+    print key,value
+for key, value in superheros.iteritems():
+        print key * 3,value
 
 
-context = {
-    'questions': [
-    {'id':1, 'content': "Why is there a light in the fridge and not in the freezer?"},
-    {'id':2, 'content': "Why don't sheep shrink when it rains?"},
-    {'id':3, 'content': "Why are they called apartments when they are all stuck together?"},
-    {'id':4, 'content': "Why do cars drive on the parkway and park on the driveway?"}
-    ]
+print "**------------------**"
+#lists in dictionaries
+superheroes = {
+    "quickster": ["The Flash", "Quick Silver"],
+    "buster": ["The Hulk", "The Thing"],
+    "flyer": ["Silver Surfer", "Iron Man", "Superman"],
+    "detective": ["Bat Man", "The Owl"],
 }
 
-for key, data in context.items():
-    for value in data:
-        print "Question #",value["id"],":",value["content"]
-        print "-------"
+for hero_type, heroes in superheroes.items():
+    print hero_type
+    for hero in heroes:
+        print ">>>>>", hero
+print "**------------------**"
 
-
-name_dictionary = {
-    "sw":"Sara Wong",
-    "mp":"Martin Puryear"
-    }
-for key in name_dictionary:
-    print name_dictionary[key]
+hero_string = "silver surfer Iron Man Superman"
+# returns the spot that the word surfer starts in the string.
+#first instance of the pattern. 
+print hero_string.find("surfer")
