c335f1a220d4a37d97d764e218588bdc67062b96
Robert Pandy <rspandy@gmail.com>
2017-03-11 15:40:30 -0600

    python updates

--- Python/form_test/server.py
+++ Python/form_test/server.py
@@ -6,12 +6,17 @@ def index():
     return render_template("index.html")
 #this route will handle our form submission
 # notice how we defined which HTTP methods are allowed by this route
+# the server is listening for a POST request to:
+# localhost:5000/users
+# we define the route below such that the route matches the action of our own form. - '/users'
+# similarly we need to allow specific methods - POST in this case
 @app.route('/users', methods=['POST'])
 def create_user():
     print "Got Post Info"
-    #we'll talk about the following two lines after we learn some more
+    #recall the name attributes we added to our form inputs
+    # to access the data that the user input into the fields we use request.form['name_of_input']
     name = request.form['name']
     email = request.form['email']
     #redirects back to the '/' route
     return redirect('/')
-app.run(debug=True) #runs our server 
+app.run(debug=True) #runs our server redirects back to the '/' route
