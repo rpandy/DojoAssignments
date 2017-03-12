da325107347961f5d6a580abaee06376b733b4dd
Robert Pandy <rspandy@gmail.com>
2017-03-12 13:12:11 -0500

    flask updates w/ Valentyna

--- Python/dojo_survey/server.py
+++ Python/dojo_survey/server.py
@@ -8,15 +8,17 @@ def index():
 @app.route('/result', methods=["POST"])
 def survey_submission():
     #print request.form
+    user = request.form
+    print user['name']
     name = request.form['name']
     location = request.form['location']
     fav_language = request.form['fav_language']
     comments = request.form['comments']
     #return render_template("results.html", context = {name,location,fav_language,comments})
     #using jinja context dictionary to send variables to the HTML template
-    return render_template("results.html", name = name, location = location, fav_language = fav_language, comments = comments)
-    return redirect('/')
+    #return render_template("results.html", name = name, location = location, fav_language = fav_language, comments = comments)
+    return render_template("results.html", user=user)
 # @app.route('/')
 # def goback():
-#      return redirect("index.html")
+#      return redirect('/')
 app.run(debug=True, port=8000)
