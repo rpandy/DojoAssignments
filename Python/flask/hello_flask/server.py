a1ad76d9f9d546074df5af549359ef765f5b6778
Robert Pandy <rspandy@gmail.com>
2017-03-15 09:11:07 -0500

    added additional notes to basic flask file

--- Python/flask/hello_flask/server.py
+++ Python/flask/hello_flask/server.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request, redirect, session, flash # Import Flask to allow us to create our app, and import
+# Import Flask to allow us to create our app, and import
+# render_template to allow us to render index.html.
+from flask import Flask, render_template, request, redirect, session, flash
+# Global variable __name__ tells Flask whether or not we
+# are running the file directly or importing it as a module.
 app = Flask(__name__)
 app.secret_key = "I<3Secrets"
 
 
 #each route should do one thing.
 #Either post information or gather information to send elsewhere
+# The "@" symbol designates a "decorator" which attaches the
+# following function to the '/' route. This means that
+# whenever we send a request to localhost:5000/ we will run the following function
 @app.route('/')
 def index():
     if "counter" not in session:
@@ -13,7 +20,8 @@ def index():
     age = 33
     job = "coder"
     #name you expect variable to be on template and what the variable will be equal to
-    
+
+    # Render the template and return it!
     return render_template('index.html',age = age, job=job)
 
 @app.route('/users', methods = ["POST"])
