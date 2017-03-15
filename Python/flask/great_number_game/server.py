2ccd0b19a58cc3b2d69a7f71d1ce50b5ff58c6d8
Robert Pandy <rspandy@gmail.com>
2017-03-15 10:20:06 -0500

    flask updates

--- Python/flask/great_number_game/server.py
+++ Python/flask/great_number_game/server.py
@@ -10,13 +10,9 @@ random_number = random.randrange(1,101)
 
 @app.route('/')
 def index():
-    #import random
-    #random_number = random.randrange(0,101)
     #print random_number
     session['random'] = random_number
-    session['guess'] = 0
-    print 'the randomly generated number is:', session['random']
-
+    #print 'the randomly generated number is:', session['random']
 
     return render_template('index.html', random = session['random'])
 
@@ -24,9 +20,12 @@ def index():
 @app.route('/', methods=['POST'])
 def guess_submission():
     #print request.form
+    # session['guess'] = 0
     session['guess'] = request.form['guess']
-    guess = session['guess']
+    #guess = session['guess']
     print 'the guess is:', session['guess']
+    print 'the randomly generated number is:', session['random']
+
     return redirect('/')
 
 
@@ -39,23 +38,25 @@ def test_logic():
     random = session['random']
     guess = session['guess']
 
-    # if random < guess:
-    #     print 'testing testing 123'
-    # elif random > guess:
-    #     print 'hahahahahahahahahah'
-    # else:
-    #     print 'CORRRRRRRRRECT'
-
     if random < guess:
-        session['answer'] = 'guess is too high'
-        print session['answer']
+        print 'random < guess'
     elif random > guess:
-        session['answer'] = 'guess is too low'
-        print session['answer']
+        print 'random > guess'
+    else:
+        print 'random = guess'
+
+    # if random < guess:
+    #     session['answer'] = 'guess is too high'
+    #     print session['answer']
+    # elif random > guess:
+    #     session['answer'] = 'guess is too low'
+    #     print session['answer']
     return redirect('/')
 #if statement comparing the guess to session['random']
 @app.route('/clear')
 def clear():
     session.clear()
+    #guess is set to 0 after session is cleared
+    session['guess'] = 0
     return redirect('/')
 app.run(debug=True)
