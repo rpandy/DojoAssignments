6db44b66e40231e15c94adc92c78b89c3a9104e0
Robert Pandy <rspandy@gmail.com>
2017-03-20 08:08:25 -0500

    adjustments

--- Python/flask/disappearing_ninja/server.py
+++ Python/flask/disappearing_ninja/server.py
@@ -16,19 +16,21 @@ def four_turtles():
         'red': 'Raphael',
         'orange': 'Michelangelo'
     }
-
-    main_page = True
-    print "this is the main page", main_page
-    print "the ninja_color dictionary is the following:", ninja_color
-    print "The value for the key:blue is:", ninja_color['blue']
+    #attempting to combine templates.
+    #When main_page = TRUE then we show all four turtles
+    #When main_page = FALSE we execute the code on the one_character template
+    main_page = 'True'
+    #print "this is the main page", main_page
+    #print "the ninja_color dictionary is the following:", ninja_color
+    #print "The value for the key:blue is:", ninja_color['blue']
     session['ninja_details'].append(ninja_color)
     print "list items are indexed... at index 0", session['ninja_details']
-    return render_template('/ninja.html', turtles = ninja_color)
+    return render_template('/ninja.html', turtles = ninja_color, main_page=main_page)
 
 @app.route('/ninja/<ninja_id>')
 def one_turtle(ninja_id):
     print "this is the ninja_id:", ninja_id
-    main_page = False
+    main_page = 'False'
     print "this is the main page", main_page
 
 
