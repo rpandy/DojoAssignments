c7d35cd03af700d990f558d81199677119ec226f
Robert Pandy <rspandy@gmail.com>
2017-03-20 09:43:33 -0500

    added notes

--- Python/flask/disappearing_ninja/server.py
+++ Python/flask/disappearing_ninja/server.py
@@ -4,12 +4,14 @@ app.secret_key = "SECRETSSSS"
 
 @app.route('/')
 def index():
+    #create a list in session if list isn't created already
     if "ninja_details" not in session:
         session['ninja_details'] = []
     return render_template('index.html')
 
 @app.route('/ninja')
 def four_turtles():
+    # creat dictionary of characters
     ninja_color = {
         'blue': 'Leonardo',
         'purple': 'Donatello',
@@ -17,24 +19,27 @@ def four_turtles():
         'orange': 'Michelangelo'
     }
     #attempting to combine templates.
-    #When main_page = TRUE then we show all four turtles
-    #When main_page = FALSE we execute the code on the one_character template
+        #When main_page = TRUE then we show all four turtles
+        #When main_page = FALSE we execute the code on the one_character template
     main_page = 'True'
-    #print "this is the main page", main_page
+    print "this is the main page:", main_page
     #print "the ninja_color dictionary is the following:", ninja_color
     #print "The value for the key:blue is:", ninja_color['blue']
     session['ninja_details'].append(ninja_color)
     print "list items are indexed... at index 0", session['ninja_details']
+    #pass variables to ninja.html template
     return render_template('/ninja.html', turtles = ninja_color, main_page=main_page)
 
+#use the <ninja_id> variable to specify specific character
 @app.route('/ninja/<ninja_id>')
-def one_turtle(ninja_id):
-    print "this is the ninja_id:", ninja_id
+def one_turtle(ninja_id): # <-- pass variable into function
+    #print "this is the ninja_id:", ninja_id
     main_page = 'False'
     print "this is the main page", main_page
-
-
+    #pass ninja_id to one_character template
     return render_template('/one_character.html', ninja_id = ninja_id)
+
+#clear session
 @app.route('/clear')
 def clear_session():
     session.clear()
