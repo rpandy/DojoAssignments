e8208348c4d0cc079687028a3998f993dc612d8a
Robert Pandy <rspandy@gmail.com>
2017-03-25 16:29:02 -0500

    added update route

--- Python/flask_mysql/full_friends/server.py
+++ Python/flask_mysql/full_friends/server.py
@@ -27,5 +27,31 @@ def create():
 
     mysql.query_db(insert_query,data)
     return redirect('/')
+@app.route('/friends/<friend_id>/edit')
+def edit(friend_id):
+    query = "SELECT * FROM friends"
+    friends = mysql.query_db(query)
+    
+    edit_query = "SELECT * FROM friends WHERE id = :id"
+
+    data = {'id': friend_id}
+
+    friends = mysql.query_db(edit_query,data)
+    return render_template('friends.html', friends=friends)
+
+@app.route('/friends/<friend_id>', methods=['POST'])
+def update(friend_id):
+
+    update_query = "UPDATE friends SET first_name = :first_name, last_name = :last_name, occupation = :occupation WHERE id = :id"
+
+    data = {
+    'first_name': request.form['first_name'],
+    'last_name': request.form['last_name'],
+    'occupation': request.form['occupation'],
+    'id': friend_id
+    }
+
+    mysql.query_db(update_query,data)
+    return redirect('/')
 
 app.run(debug=True, port=8000)
