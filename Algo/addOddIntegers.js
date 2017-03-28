bee2d25308a6041d923e990ae8bd622bb63b0d0b
Robert Pandy <rspandy@gmail.com>
2017-03-28 09:44:02 -0500

    algo practice

--- Algo/addOddIntegers.js
+++ Algo/addOddIntegers.js
@@ -1,13 +1,26 @@
 //Whoa, That Sucker’s Huge…
 //Add odd integers from -300,000 to 300,000, and console.log the final sum. Is there a shortcut?
+// function addOddIntegers() {
+// var sum = 0
+//     for(var i = -300000; i<=300000; i++){
+//         if(i % 2 !== 0){
+//             sum+=i
+//         }
+//     }
+//     console.log(sum);
+// }
+//
+// addOddIntegers();
+
 function addOddIntegers() {
-var sum = 0
-    for(var i = -300000; i<=300000; i++){
-        if(i % 2 !== 0){
-            sum+=i
+    var sum = 0
+
+    for (var i = -300000; i <= 300000; i++) {
+        if (i % 2 != 0) {
+            sum += i
         }
     }
-    console.log(sum);
+    console.log(sum)
 }
 
-addOddIntegers();
+addOddIntegers()
