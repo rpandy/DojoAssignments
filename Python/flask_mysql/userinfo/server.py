1f2bbf92a2981374aaf6d429c96a3e4bdbd43cfb
Robert Pandy <rspandy@gmail.com>
2017-03-29 14:16:01 -0500

    updated usersinfo example

--- Python/flask_mysql/userinfo/server.py
+++ Python/flask_mysql/userinfo/server.py
@@ -1,34 +1,117 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session,flash
 from mysqlconnection import MySQLConnector
+from bcrpyt import Bcrypt
+import re
 app = Flask(__name__)
 app.secret_key = "SEEEECRETTTS"
 mysql = MySQLConnector(app, 'userinfo')
 
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+
 @app.route('/')
 def index():
-    users = mysql.query_db('SELECT * FROM users')
-    return render_template('index.html', user_info=users)
+    # users = mysql.query_db('SELECT * FROM users')
+    return render_template('index.html')
 
 
 @app.route('/user', methods=['POST'])
-def addUser():
-    newUser_query = "INSERT INTO users(first_name, last_name, created_at, updated_at)VALUES(:first_name, :last_name, now(), now());"
-    #cleans up the data by making it a literal string.
-    data = {
-        'first_name': request.form['fname'],
-        'last_name': request.form['lname']
-    }
-    newUserId = mysql.query_db(newUser_query, data)
-    session['userinfo'] = data
-    session['userinfo']['user_id'] = newUserId
+def logreg():
+    #checking the name from the forms to determine
+    #whether we are registering or logging in.
+    if "register" in request.form:
+    #checking to see if email exist
+    #to protect from sql injection we pass
+    #email from form into a data variable
+    #simply entering request.form['email'] would leave us exposed.
+        user_query = "SELECT id FROM users WHERE email = :email"
+        user_data = {
+            'email': request.form['email']
+        }
+        #this will return a list based on mysqlconnection file.
+        #if nothing is found we get an empty list
+        userExists = mysql.query_db(user_query, user_data)
+        print "This is the if userExists query:",userExists
+        #check if there is actually a value in userExists w/ conditionals.
+        #dont let the user know that email is already in database.
+        #use a more cryptic message
+        if userExists:
+            flash("Email address is invalid. Please use another address")
+            print user_data['email']
+            print userExists
+        #email validation
+        else:
+            if not EMAIL_REGEX.match(request.form['email']):
+                flash("Email must be a valid email")
+            if len(request.form['fname']) < 2:
+                flash("First Name must be longer than two characters")
+            if len(request.form['lname']) < 2:
+                flash("Last Name must be longer than two characters")
+            if len(request.form['password']) < 8:
+                flash("Password must be longer than 8 characters")
+            #ran check to see items saved in session
+            # for item in session:
+            #     print item
 
-    # {
-    # 'first_name': request.form['fmame'],
-    # 'last_name': request.form['lname'],
-    # 'user_id': newUserId
-    # }
+            #statement to determine whether we have messages in flash
+            #flash messages are saved in session.
+            # 'underbar' flashes in session
+        if '_flashes' in session:
+            return redirect('/')
 
-    # print sesion['userinfo']
+        #if email is valid we register them
+        new_user_query = "INSERT INTO users(first_name, last_name, email, pass, created_at, updated_at)VALUES(:first_name, :last_name, :email, :pass, now(), now())"
+        #cleans up the data by making it a literal string.
+        new_user_data = {
+            'first_name': request.form['fname'],
+            'last_name': request.form['lname'],
+            'email': request.form['email'],
+            'pass': request.form['password']
+        }
+
+
+        newUserId = mysql.query_db(new_user_query, new_user_data)
+        # do not save the password to session
+        # so we build out user info instead of setting new_user_data to session['userinfo']
+        # we need to save this info into session so we dont need to make multiple database queries,
+        # we can save settings and we can track the user on multiple pages
+        flash('You are now registered')
+        return redirect('/')
+
+#2 methods on the same route. in order to differentiate we can use  names in the forms. SEE ABOVE (BEFORE REGISTRATION)
+    elif 'login' in request.form:
+        #SQL query is doing the comparison to check whether email and password match
+        user_query = "SELECT id, first_name, last_name, email, pass FROM users WHERE email = :email AND pass = :pass"
+
+        user_data = {
+        'email': request.form['email'],
+        'pass': request.form['password'],
+        }
+
+        #this will always be a list of something or an empty list
+        checkUser = mysql.query_db(user_query,user_data)
+        print "CheckUser is:",checkUser
+        #weuse a conditional
+        #if its correct we login the user. if incorrect name OR password is incorrect.
+        #for security reasons we dont tell them what they have right or wrong
+        if not checkUser:
+            flash('Email or Password is invalid')
+            return redirect('/')
+            # since this is a list we need to use [0] to access that list
+        session['userinfo'] = {
+            'first_name': checkUser[0]['first_name'],
+            'last_name': checkUser[0]['last_name'],
+            'email': checkUser[0]['email'],
+            'id': checkUser[0]['id']
+        }
+        return redirect('/home')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
     return redirect('/')
 
 app.run(debug=True, port=8000)
