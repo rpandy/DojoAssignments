68e9a098a47a1f2f74a2d7f1a3e4d97f59d4102b
Robert Pandy <rspandy@gmail.com>
2017-04-18 11:55:34 -0500

    initial commit

---
+++ Python/django/secrets/apps/secrets_app/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+# class Secret(models.Model):
+#     secret = models.TextField()
+#     likes = models.IntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
+#     objects = SecretManager()
+# class Like(models.Model)
